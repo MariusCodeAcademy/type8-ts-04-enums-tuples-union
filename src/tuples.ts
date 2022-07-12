045e57ef27fcbf14f83da40d0d6a1e3819b39ddf
Marius <marius@example.com>
2022-07-12 08:59:45 +0300

    uzd

---
+++ src/tuples.ts
@@ -0,0 +1,13 @@
+// Tuple - grieztai aprsasytas masyvas
+
+const person: [number, string, boolean] = [50, 'James', true];
+
+type TupTypeArr = [boolean, string][];
+
+const tupleArr: TupTypeArr = [
+  [true, 'London'],
+  [false, 'Viena'],
+  [false, 'Hamburg'],
+];
+
+console.log('tupleArr ===', tupleArr);
