30c82f195fdfc8f095a3e133d6f901c7001f9ded
Marius <marius@example.com>
2022-07-12 11:59:41 +0300

    end

--- src/htmlEnum.ts
+++ src/htmlEnum.ts
@@ -2,13 +2,27 @@
 
 const destEl = document.getElementById('app') as HTMLDivElement | null;
 
+// type StateType = 'Loading...' | 'Error' | 'Online';
+
 enum AppState {
   Loading = 'Loading...',
-  Error = 'error',
+  Error = 'Error!!!',
   Online = 'online',
 }
 
+// interface BlueInt {
+//   state: AppState;
+//   kitaState: StateType;
+// }
+
+function clearEl(dest: HTMLDivElement | null): void {
+  if (!dest) return; // exit clause
+  // eslint-disable-next-line no-param-reassign
+  dest.innerHTML = '';
+}
+
 function runApp(state: AppState): void {
+  clearEl(destEl);
   switch (state) {
     case AppState.Loading:
       console.log('Loading');
@@ -20,19 +34,26 @@ function runApp(state: AppState): void {
       break;
     case AppState.Online:
       console.log('online ');
+      outputState(AppState.Online);
       break;
     default:
       console.log('state not found: ');
   }
 }
-runApp(AppState.Loading);
 
 function outputState(state: AppState): void {
   const cardEl: HTMLDivElement = document.createElement('div');
-  cardEl.className = 'card';
+  cardEl.className = state === AppState.Error ? 'card error' : 'card';
   const titleEl: HTMLHeadingElement = document.createElement('h2');
-  titleEl.textContent = state;
+  titleEl.textContent = state === AppState.Online ? 'Serbentaudas Bordiuras pasiruoses' : state;
   cardEl.appendChild(titleEl);
   if (!destEl) return;
   destEl.append(cardEl);
 }
+
+//  run app
+
+runApp(AppState.Loading);
+setTimeout(() => {
+  runApp(AppState.Online);
+}, 5000);
